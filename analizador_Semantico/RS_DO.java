639491132a68d8e7af15639e9df7edb4315a3432
Diego Armando Guzmán Villalta <33551960+Diego-Guzman@users.noreply.github.com>
2017-11-27 16:15:24 -0600

    Add files via upload
    clases RS, RS_DO y RS_Operador nuevas

---
+++ analizador_Semantico/RS_DO.java
@@ -0,0 +1,18 @@
+package analizador_Semantico;
+
+public class RS_DO extends Registro_Semantico{
+    private String tipo;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public RS_DO() {
+    }
+    
+    
+}
